09fb34547ae2787da1a3f1bb651b291eeb73f7ea
Mark Calcagno <mcalcagn@gmail.com>
2023-02-18 21:18:33 -0600

    Added friend feature to users

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,9 +33,9 @@ module.exports = {
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
-          : Student.deleteMany({ _id: { $in: user.students } })
+          : User.deleteMany({ _id: { $in: user.users } })
       )
-      .then(() => res.json({ message: 'User and students deleted!' }))
+      .then(() => res.json({ message: 'User and users deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
   // Update a user
@@ -53,37 +53,37 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
-    // Add an friend to a student
+    // Add an friend to a user
   addFriend(req, res) {
     console.log('You are adding an friend');
     console.log(req.body);
-    Student.findOneAndUpdate(
-      { _id: req.params.studentId },
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
       { $addToSet: { friends: req.body } },
       { runValidators: true, new: true }
     )
-      .then((student) =>
-        !student
+      .then((user) =>
+        !user
           ? res
               .status(404)
-              .json({ message: 'No student found with that ID :(' })
-          : res.json(student)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Delete friend from a student
+  // Delete friend from a user
   deleteFriend(req, res) {
-    Student.findOneAndUpdate(
-      { _id: req.params.studentId },
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
       { $pull: { friend: { friendId: req.params.friendId } } },
       { runValidators: true, new: true }
     )
-      .then((student) =>
-        !student
+      .then((user) =>
+        !user
           ? res
               .status(404)
-              .json({ message: 'No student found with that ID :(' })
-          : res.json(student)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
